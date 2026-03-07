0f14d4c2e5db142a9831a99329e5c6b22fcb278d
alizonekreate <alsnkte@gmail.com>
2026-03-07 14:27:23 +0800

    Remove p3-js-api-app and p4-node-app

--- p4-node-app/models/motivationModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const motivationSchema = new mongoose.Schema({
-    journalId: { type: mongoose.Schema.Types.ObjectId, ref: "Journal", required: true },
-    text: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model("Motivation", motivationSchema);
